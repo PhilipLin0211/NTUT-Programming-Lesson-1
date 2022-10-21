cc5ac4ae7a6bf8094400f4ceb8d0e37b0c26f1ca
PhilipLin0211 <philip.lin0211@gmail.com>
2022-10-21 14:35:32 +0800

    HW

---
+++ Homework 3/3-1/p13/Untitled2.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+double Power(double,int);
+void main(int)
+{
+	int k;
+	double Ans;
+	printf("計算3.5的k次方?請輸入k=");
+	scanf("%d", &k);
+	Ans = Power(3.5,k);
+	system("pause");
+	return 0;
+}
+
+double Power(double X,int n)
+{
+	int i;
+	double PowerXn = 1;
+	for(i = 1; i <= n; i++)
+	{
+		PowerXn = PowerXn*X;
+	}
+	return PowerXn;
+}
