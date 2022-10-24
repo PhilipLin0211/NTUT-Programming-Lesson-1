0b71ddcf6f897a24324abda49ce30f8cc742740a
PhilipLin0211 <philip.lin0211@gmail.com>
2022-10-24 17:26:49 +0800

    edit

---
+++ Homework 3/3-2/recrusive exponentiation/新文件2.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+double intpower(double x, int n)
+{
+    double result;
+    if(n>1&&n%2!=0) {result=x*intpower(x,(n-1)/2)*intpower(x,(n-1)/2);}
+    if(n>1&&n%2==0) {result=intpower(x,n/2)*intpower(x,n/2);}
+    if(n==1) return x;
+    else return result;
+}
+
+
+int main()
+{
+    int n;
+    double x,result;
+    printf("x\n");
+    scanf("%lf", &x);
+    printf("n\n");
+    scanf("%d", &n);
+    printf("result = %.2f\n", intpower(x,n));
+
+
+    return 0;
+}
